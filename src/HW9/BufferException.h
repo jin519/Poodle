18b2d9b01642867d2a6ec7730dc1909e104bdc6e
Jin Won <godjin519@gmail.com>
2020-05-04 22:44:10 +0900

    HW9 (#17)
    * HW9
    
    * HW9 sein feedback

---
+++ src/HW9/BufferException.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <exception>
+
+class BufferException : public std::exception
+{
+public:
+	/* constructor */
+	BufferException(const std::string& message) noexcept;
+
+	/* member function */
+	virtual const char* what() const noexcept override;
+
+private:
+	/* member variable */
+	const std::string __message;
+};
